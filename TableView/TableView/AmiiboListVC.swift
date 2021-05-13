cc98b2d4c59669b5e0e44d6de6a27bd39fe27a5b
StartingCoding <StartingCodingPartner@gmail.com>
2021-05-13 12:20:37 +0200

    Setup for UITableView in the ViewController

--- TableView/TableView/AmiiboListVC.swift
+++ TableView/TableView/AmiiboListVC.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class AmiiboListVC: UIViewController {
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        setupView()
+    }
+    
+    func setupView() {
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
 }
 
