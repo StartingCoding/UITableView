b2d7861ee336173eae69c04ea4e9a54d1df721da
StartingCoding <StartingCodingPartner@gmail.com>
2021-05-13 11:01:37 +0200

    Started project

---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Loris on 13/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
