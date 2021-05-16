0abc3deda8e8325313bb3b993f93f3d0b8ccb21a
StartingCoding <StartingCodingPartner@gmail.com>
2021-05-16 17:36:09 +0200

    Created NSCache to reduce dataTasks of images in AmiiboCell

--- TableView/TableView/CustomImageView.swift
+++ TableView/TableView/CustomImageView.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let imageCache = NSCache<AnyObject, AnyObject>()
+
 class CustomImageView: UIImageView {
     // Global variable to hold the dataTask
     var task: URLSessionDataTask!
@@ -20,18 +22,27 @@ class CustomImageView: UIImageView {
             task.cancel()
         }
         
+        // If the image from the url is in the Cache use that instead
+        if let imageFromCache = imageCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
+            self.image = imageFromCache
+            return
+        }
+        
         task = URLSession.shared.dataTask(with: url) { (data, resp, error) in
             
             guard
                 let data = data,
-                let newIamge = UIImage(data: data)
+                let newImage = UIImage(data: data)
             else {
                 print("Couldn't load image from url: \(url)")
                 return
             }
             
+            // Saving the image with an unique identifier base on the url of the image
+            imageCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
+            
             DispatchQueue.main.async {
-                self.image = newIamge
+                self.image = newImage
             }
         }
         
