3c7e6436e1f7a46830459096b5be07bd304c99cd
StartingCoding <StartingCodingPartner@gmail.com>
2021-05-16 16:06:36 +0200

    Created ImageView in custom cell programmatically

--- TableView/TableView/AmiiboCell.swift
+++ TableView/TableView/AmiiboCell.swift
@@ -7,4 +7,33 @@
 
 import UIKit
 
-class AmiiboCell: UITableViewCell {}
+class AmiiboCell: UITableViewCell {
+    let imageIV = UIImageView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Setup
+    func setupView() {
+        setupImageView()
+    }
+    
+    func setupImageView() {
+        addSubview(imageIV)
+        
+        imageIV.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageIV.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            imageIV.centerYAnchor.constraint(equalTo: centerYAnchor),
+            imageIV.widthAnchor.constraint(equalToConstant: 40),
+            imageIV.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        imageIV.backgroundColor = .red
+    }
+}
