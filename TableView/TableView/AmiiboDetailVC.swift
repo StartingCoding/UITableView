cd4ca2c6682478d0ceaf070e7f179a4baa920f4f
StartingCoding <StartingCodingPartner@gmail.com>
2021-06-10 18:33:57 +0200

    Created CustomImageView in AmiiboDetailVC

--- TableView/TableView/AmiiboDetailVC.swift
+++ TableView/TableView/AmiiboDetailVC.swift
@@ -10,9 +10,33 @@ import UIKit
 class AmiiboDetailVC: UIViewController {
     var amiibo: AmiiboForView?
     
+    let imageIV = CustomImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .green
+        
+        setupImage()
+        setupData()
+    }
+    
+    func setupImage() {
+        view.addSubview(imageIV)
+        imageIV.translatesAutoresizingMaskIntoConstraints = false
+        imageIV.contentMode = .scaleAspectFit
+        
+        NSLayoutConstraint.activate([
+            imageIV.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            imageIV.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageIV.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.5),
+            imageIV.heightAnchor.constraint(equalTo: imageIV.widthAnchor)
+        ])
+    }
+    
+    func setupData() {
+        if let amiibo = amiibo,
+           let url = URL(string: amiibo.imageUrl) {
+            imageIV.loadImage(from: url)
+        }
     }
 }
